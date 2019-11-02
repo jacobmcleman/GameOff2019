3d84a946766b2dd1ee2527c9b84c01718fafab30
Jake McLeman <jake@mcleman.com>
2019-11-01 22:26:30 -0700

    its a blue circle and it moves!

--- jam_game/src/main.rs
+++ jam_game/src/main.rs
@@ -4,37 +4,66 @@ use quicksilver::{
     Result,
     geom::{Circle, Line, Rectangle, Transform, Triangle, Vector},
     graphics::{Background::Col, Color},
+    input::{Key},
     lifecycle::{Settings, State, Window, run}
 };
 
-struct DrawGeometry;
+struct DrawObjects {
+    player: GameObject
+}
 
-impl State for DrawGeometry {
-    fn new() -> Result<DrawGeometry> {
-        Ok(DrawGeometry)
+enum SpriteShape {
+    Circle,
+    Rectangle
+}
+
+struct GameObject {
+    position: Vector,
+    rotation: f32,
+    scale: Vector,
+    shape: SpriteShape,
+    color: Color
+}
+
+impl GameObject {
+    fn draw(&mut self, window: &mut Window) {
+        match self.shape {
+            SpriteShape::Circle => window.draw(&Circle::new(self.position, self.scale.x), Col(self.color)),
+            SpriteShape::Rectangle => window.draw(&Rectangle::new(self.position, self.scale), Col(self.color))
+        }
+    }
+}
+
+impl State for DrawObjects {
+    fn new() -> Result<DrawObjects> {
+        let player = GameObject { position: Vector::new(100, 100), rotation: 0.0, scale: Vector::new(100, 100), shape: SpriteShape::Circle, color: Color::BLUE};
+        Ok(DrawObjects {player} )
     }
 
     fn draw(&mut self, window: &mut Window) -> Result<()> {
         window.clear(Color::WHITE)?;
-        window.draw(&Rectangle::new((100, 100), (32, 32)), Col(Color::BLUE));
-        window.draw_ex(&Rectangle::new((400, 300), (32, 32)), Col(Color::BLUE), Transform::rotate(45), 10);
-        window.draw(&Circle::new((400, 300), 100), Col(Color::GREEN));
-        window.draw_ex(
-            &Line::new((50, 80),(600, 450)).with_thickness(2.0),
-            Col(Color::RED),
-            Transform::IDENTITY,
-            5
-        );
-        window.draw_ex(
-            &Triangle::new((500, 50), (450, 100), (650, 150)),
-            Col(Color::RED),
-            Transform::rotate(45) * Transform::scale((0.5, 0.5)),
-            0
-        );
+        self.player.draw(window);
+        Ok(())
+    }
+
+    fn update(&mut self, window: &mut Window) -> Result<()> {
+        if window.keyboard()[Key::Left].is_down() {
+            self.player.position.x -= 1.0;
+        }
+        if window.keyboard()[Key::Right].is_down() {
+            self.player.position.x += 1.0;
+        }
+        if window.keyboard()[Key::Down].is_down() {
+            self.player.position.y += 1.0;
+        }
+        if window.keyboard()[Key::Up].is_down() {
+            self.player.position.y -= 1.0;
+        }
+
         Ok(())
     }
 }
 
 fn main() {
-    run::<DrawGeometry>("Geometry Test", Vector::new(800, 600), Settings::default());
+    run::<DrawObjects>("Game Test", Vector::new(800, 600), Settings::default());
 }
