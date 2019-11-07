eb0a753784d80e2a1aeb9ccd93d69c6540f23822
Jake McLeman <jake@mcleman.com>
2019-11-07 01:34:28 -0800

    Improved benchmarking suite a lot

--- jam_game/tilemap/benches/map_perf_bench.rs
+++ jam_game/tilemap/benches/map_perf_bench.rs
@@ -7,7 +7,7 @@ use criterion::black_box;
 use rand::Rng;
 
 use tilemap::tile_world::{
-    TileMap, GridCoord
+    TileMap, GridCoord, TileValue
 };
 
 
@@ -15,9 +15,32 @@ fn criterion_benchmark(c: &mut Criterion) {
     let mut world = TileMap::new();
     let mut rng = rand::thread_rng();
 
-    c.bench_function("dense_map_access_random", |b| b.iter(|| world.sample(&GridCoord{x: rng.gen::<i64>() % 16, y: rng.gen::<i64>() % 16})));
-    c.bench_function("sparse_map_access_random", |b| b.iter(|| world.sample(&GridCoord{x: black_box(0), y: black_box(0)})));
-    c.bench_function("map_access_repeated", |b| b.iter(|| world.sample(&GridCoord{x: rng.gen::<i64>() % 16, y: rng.gen::<i64>() % 16})));
+    c.bench_function("map_read_repeated", |b| b.iter(|| world.sample(&GridCoord{x: black_box(0), y: black_box(0)})));
+    c.bench_function("map_write_repeated", |b| b.iter(|| world.make_change(&GridCoord{x: black_box(0), y: black_box(0)}, &TileValue::Error)));
+    c.bench_function("read_write_read_repeated", |b| b.iter(|| {
+        let coord = GridCoord{x: rng.gen::<i64>(), y: rng.gen::<i64>()};
+        world.sample(&coord);
+        world.make_change(&coord, &TileValue::Error);
+        world.sample(&coord);
+    }));
+
+    c.bench_function("dense_map_read_random", |b| b.iter(|| world.sample(&GridCoord{x: rng.gen::<i64>() % 16, y: rng.gen::<i64>() % 16})));
+    c.bench_function("dense_map_write_random", |b| b.iter(|| world.make_change(&GridCoord{x: rng.gen::<i64>() % 16, y: rng.gen::<i64>() % 16}, &TileValue::Error)));
+    c.bench_function("dense_map_read_write_read_random", |b| b.iter(|| {
+        let coord = GridCoord{x: rng.gen::<i64>() % 16, y: rng.gen::<i64>() % 16};
+        world.sample(&coord);
+        world.make_change(&coord, &TileValue::Error);
+        world.sample(&coord);
+    }));
+
+    c.bench_function("sparse_map_read_random", |b| b.iter(|| world.sample(&GridCoord{x: rng.gen::<i64>(), y: rng.gen::<i64>()})));
+    c.bench_function("sparse_map_write_random", |b| b.iter(|| world.make_change(&GridCoord{x: rng.gen::<i64>(), y: rng.gen::<i64>()}, &TileValue::Error)));
+    c.bench_function("sparse_map_read_write_read_random", |b| b.iter(|| {
+        let coord = GridCoord{x: rng.gen::<i64>(), y: rng.gen::<i64>()};
+        world.sample(&coord);
+        world.make_change(&coord, &TileValue::Error);
+        world.sample(&coord);
+    }));
 }
 
 criterion_group!(benches, criterion_benchmark);
